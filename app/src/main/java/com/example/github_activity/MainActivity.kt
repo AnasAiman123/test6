febc1e00ed57b66c4947a911b3bcab594b1b8321
AnasAiman123 <anasaiman1233@gmail.com>
2023-03-02 20:08:50 +0200

    FComment

---
+++ app/src/main/java/com/example/github_activity/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.github_activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    val db = Firebase.firestore
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            var name = editTextTextPersonName.text.toString()
+            var id = editTextTextPersonName2.text.toString()
+            var password = editTextTextPersonName3.text.toString()
+            val user = hashMapOf(
+                "name" to name,
+                "id" to id,
+                "password" to password
+            )
+            db.collection("users")
+                .add(user)
+                .addOnSuccessListener { documentReference ->
+                    Log.d("TAG", "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w("TAG", "Error adding document", e)
+                }
+        }
+    }
+}
