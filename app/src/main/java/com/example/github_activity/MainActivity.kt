d9ea8035f1253dc2b7845490aacb8da59c86c1ae
AnasAiman123 <anasaiman1233@gmail.com>
2023-03-02 20:20:55 +0200

    SComment

--- app/src/main/java/com/example/github_activity/MainActivity.kt
+++ app/src/main/java/com/example/github_activity/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //save to firebase
         button.setOnClickListener {
             var name = editTextTextPersonName.text.toString()
             var id = editTextTextPersonName2.text.toString()
